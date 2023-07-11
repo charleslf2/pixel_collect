560868220d82f3c2e972c60a2b83543288842a84
charleslf2 <datadevfernolf@gmail.com>
2023-07-11 08:01:21 -0700

    it works

--- pixel_collect/main.py
+++ pixel_collect/main.py
@@ -37,43 +37,46 @@ def request_to_real_site_url(i_url_list):
             pass
     return imglink_list 
 
-def download_image(imglink_list,l,sf):
+def download_image(request_url_list,l,save):
     image_link_list=[]
     nbr=0
-    for i in range(len(imglink_list)):
+    for i in range(len(request_url_list)):
         if nbr>=l:
             print("End of scrap ... exit")
             break;
         try:
-            begin=imglink_list[i][0:5]
+            begin=request_url_list[i][0:5]
             if begin =="https":
-                _,ext=os.path.splitext(imglink_list[i])
-                image_data=requests.get(imglink_list[i]).content
+                _,ext=os.path.splitext(request_url_list[i])
+                image_data=requests.get(request_url_list[i]).content
 
-                if imglink_list[i] in image_link_list:
+                if request_url_list[i] in image_link_list:
                     print("Image already existed.... Skip")
                     pass
                 else:
                     nbr+=1
-                    image_link_list.append(imglink_list[i])
-                    print("Downloading... ", imglink_list[i])
-                    img_file=open("{}/image_{:03d}{}".format(sf,ext),"wb")
+                    image_link_list.append(request_url_list[i])
+                    print("Downloading... ", request_url_list[i])
+                    img_file=open("{}/image_{:03d}{}".format(save,nbr,ext),"wb")
                     img_file.write(image_data)
                     img_file.close()
-
         except :
             print("image number " ,nbr, "save error")
             pass
 
 
+
+  
 def pixel_scrap(keyword:str,save_folder:str,limit=50,logs=False):
 
     if isinstance(keyword,str) ==False:
         raise Exception("keyword must be string")
+
     if os.path.isdir(save_folder) ==False:
         os.mkdir(save_folder)
 
-
+    save=os.path.basename(save_folder)
+    
     url="https://images.google.com/search?q="+keyword+"&tbm=isch&sxsrf"
 
     r=requests.get(url)
@@ -93,7 +96,7 @@ def pixel_scrap(keyword:str,save_folder:str,limit=50,logs=False):
 
     request_url=request_to_real_site_url(real_url)
 
-    download_image(request_url,limit,save_folder)
+    download_image(request_url,limit,save)
 
     if logs==True:
         func=open("{}/{}_scraplog.html".format(save_folder,keyword),"w",encoding="utf-8")
